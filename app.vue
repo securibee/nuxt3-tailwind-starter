b3dc8dd4492a4eb188d5a25aa73e45bbdce6c3a3
securibee <securibee@gmail.com>
2022-12-08 13:33:05 -0500

    chore: first commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-blue-700 h-screen flex">
+    <div class="self-center mx-auto max-w-2xl py-16 px-4 text-center sm:py-20 sm:px-6 lg:px-8">
+      <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+        <span class="block">Nuxt 3 + Tailwind CSS</span>
+        <span class="block text-blue-400">Beautiful & rapid prototyping</span>
+      </h2>
+      <p class="mt-4 text-lg leading-6 text-blue-200">Make your ideas come to life.</p>
+      <a href="https://nuxt.com/docs/getting-started/introduction" class="mt-8 mr-8 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-blue-600 hover:bg-blue-50 sm:w-auto">Nuxt Docs</a>
+      <a href="https://tailwindcss.com/docs/" class="mt-8 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-blue-600 hover:bg-blue-50 sm:w-auto">Tailwind Docs</a>
+      <p class="mt-6 text-blue-300">No 🐝, no 🍯.</p>
+    </div>
+  </div>
+</template>
